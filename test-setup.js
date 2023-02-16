d2aa97039fcb1cef44d5c2a1689ede4f4ee8a160
Jason Williscroft <me@karmanivero.us>
2023-02-16 14:32:51 +0800

    refined test setup

--- test-setup.js
+++ test-setup.js
@@ -5,8 +5,6 @@ import { getDotenv } from '@karmaniverous/get-dotenv';
 before(async function () {
   if (!process.env.ENV)
     await getDotenv({
-      env: 'dev',
       loadProcess: true,
-      paths: ['./', './env'],
     });
 });
