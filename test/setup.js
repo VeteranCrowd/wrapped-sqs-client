75d812daea51c1c1cb054a3fdbe480c994bbc945
Jason Williscroft <me@karmanivero.us>
2023-07-13 16:02:23 +0800

    [WEB-1059] updated dependencies

--- test/setup.js
+++ test/setup.js
@@ -1,13 +1,13 @@
 /* eslint-env mocha */
 
-import { getDotenv } from '@karmaniverous/get-dotenv';
+// npm imports
+import { testSetup } from '@veterancrowd/aws-cli';
+
+// Import version from package.json.
+import pkg from '../package.json' assert { type: 'json' };
+if (!process.env.npm_package_version)
+  process.env.npm_package_version = pkg.version;
 
 before(async function () {
-  if (!process.env.ENV)
-    await getDotenv({
-      dynamicPath: './env/dynamic.js',
-      env: 'dev',
-      loadProcess: true,
-      paths: ['./', './env'],
-    });
+  await testSetup();
 });
